71ba916a1d16a8be0936815f18b2229df05f6c06
Yenfry Herrera Feliz <yenfri01@gmail.com>
2022-08-05 14:41:24 -0700

    fix: Add a new value to x so you read better

---
+++ src/GitHubIssues/UtilityClass.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Aws\GitHubIssues;
+
+class UtilityClass
+{
+
+    // Github issue number 2345
+    public function doSomething()
+    {
+        $x = 'fixing';
+        $x .= ' a new problem';
+    }
+}
